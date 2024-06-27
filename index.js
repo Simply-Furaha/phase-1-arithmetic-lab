5178f523870ed4ee5a9314179bcc0b8cb8bf0435
Maxwell Furaha <maxwellfuraha91@gmail.com>
2024-06-27 09:19:25 +0300

    Performingg some Arithmetic Operation

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const num3 = 10;
+const num4 = 6;
+const multiply = num1 * num2;
+
+let random = Math.floor(Math.random()*5)+1;
+
+const mod = num3 % num4;
+
+let max = Math.max(2, 6, 10, 15,20,);
